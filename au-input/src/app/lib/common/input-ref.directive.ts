375c862ff01034ec89ebdceb842591702fee36a5
Joseph Joyce <joej326@gmail.com>
2018-03-12 16:33:35 -0400

    adding host listener

--- au-input/src/app/lib/common/input-ref.directive.ts
+++ au-input/src/app/lib/common/input-ref.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 // this selector says to apply this directive to allinputs within the 'app-au-fa-input' components
 @Directive({
@@ -6,6 +6,19 @@ import { Directive } from '@angular/core';
 })
 export class InputRefDirective {
 
+  focus = false;
+
   constructor() { }
+
+  @HostListener('focus')
+  onFocus() {
+    this.focus = true;
+  }
+
+  @HostListener('blur')
+  onBlur() {
+    this.focus = false;
+  }
+
 
 }
